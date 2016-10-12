1b9c2a5429815d70827868004c1825535b32af03
KJastrzebski1 <krzysztof.jastrzebski@polcode.net>
2016-10-12 17:10:00 +0200

    new functions for view

--- gloves/PluginSettings.php
+++ gloves/PluginSettings.php
@@ -12,8 +12,11 @@ class PluginSettings {
     protected static $settings = [
         'installed',
     ];
+    
+    protected static $groupName;
 
     public static function init() {
+        static::$groupName = Config::get('text-domain') . '-settings';
         add_action('admin_init', array('\Gloves\PluginSettings', 'register'));
     }
 
@@ -21,20 +24,21 @@ class PluginSettings {
      * On admin_init hook
      */
     public static function register() {
-        $domain = Config::get('text-domain') . '-settings';
+        $domain = static::$groupName;
         $slug = Config::get('text-domain');
         if (isset(static::$settings)) {
             foreach (static::$settings as $name) {
                 \register_setting($domain, $slug.'-'.$name);
             }
         }
+        
     }
 
     /**
      * On plugin deactivation
      */
     public static function unregister() {
-        $domain = Config::get('text-domain') . '-settings';
+        $domain = static::$groupName;
         $slug = Config::get('text-domain');
         if (isset(static::$settings)) {
             foreach (static::$settings as $name) {
@@ -42,6 +46,16 @@ class PluginSettings {
                 \delete_option($slug.'-'.$name);
             }
         }
+    }
+    
+    public static function generateView($id, $type, $classes=''){
+        $domain = static::$groupName;
+        $slug = Config::get('text-domain');
+        echo "<input type='$type' id='$id' name='$slug-$id' class='$classes' value='".static::get($id)."' />";
+    }
+    
+    public static function getSettingsGroup(){
+        return static::$groupName;
     }
 
     /**
@@ -58,6 +72,15 @@ class PluginSettings {
     public static function get($option) {
         $slug = Config::get('text-domain');
         return \get_option($slug.'-'.$option);
+    }
+    
+    public static function getAll(){
+        $options = array();
+        $slug = Config::get('text-domain');
+        foreach (static::$settings as $option){
+            $options[$option] = \get_option($slug.'-'.$option);
+        }
+        return $options;
     }
 
     public static function set($option, $value) {
